40f46e3e92cdd34b385631c1059842a56f06e9a5
Markus Unterwaditzer <markus-honeypot@unterwaditzer.net>
2020-11-05 00:57:32 +0100

    remove unfounded 2x multiplication

--- src/main.rs
+++ src/main.rs
@@ -346,8 +346,8 @@ fn walk_files(base_path: &Path) -> impl ParallelIterator<Item = jwalk::DirEntry<
         .collect::<Vec<_>>();
 
     // Keeping many instances of `C` alive is very memory-intense for large sites. Try to force
-    // rayon to keep only 2 per thread instead. https://github.com/rayon-rs/rayon/issues/742
-    let min_len = entries.len() / (2 * rayon::current_num_threads());
+    // rayon to keep only 1 per thread instead. https://github.com/rayon-rs/rayon/issues/742
+    let min_len = entries.len() / rayon::current_num_threads();
 
     entries.into_par_iter().with_min_len(min_len)
 }
