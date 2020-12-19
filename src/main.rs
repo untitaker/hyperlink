4cfd345d01e8c439d71f83bb715a1fe3e2fef85a
Markus Unterwaditzer <markus-honeypot@unterwaditzer.net>
2020-12-20 00:00:27 +0100

    add cli tests

--- src/main.rs
+++ src/main.rs
@@ -572,3 +572,70 @@ fn match_all_paragraphs(base_path: PathBuf, sources_path: PathBuf) -> Result<(),
 
     Ok(())
 }
+
+#[cfg(test)]
+mod tests {
+    use assert_cmd::Command;
+    use assert_fs::prelude::*;
+    use predicates::prelude::*;
+
+    #[test]
+    fn test_dead_link() {
+        let site = assert_fs::TempDir::new().unwrap();
+        site.child("index.html")
+            .write_str("<a href=bar.html>")
+            .unwrap();
+        let mut cmd = Command::cargo_bin("hyperlink").unwrap();
+        cmd.current_dir(site.path()).arg(".");
+
+        cmd.assert().failure().code(1).stdout(
+            r#"Reading files
+Checking 1 links from 1 files (1 documents)
+./index.html
+  error: bad link bar.html
+
+Found 1 bad links
+"#,
+        );
+        site.close().unwrap();
+    }
+
+    #[test]
+    fn test_dead_anchor() {
+        let site = assert_fs::TempDir::new().unwrap();
+        site.child("index.html")
+            .write_str("<a href=bar.html#goo>")
+            .unwrap();
+        site.child("bar.html").touch().unwrap();
+        let mut cmd = Command::cargo_bin("hyperlink").unwrap();
+        cmd.current_dir(site.path()).arg(".").arg("--check-anchors");
+
+        cmd.assert().failure().code(2).stdout(
+            r#"Reading files
+Checking 1 links from 2 files (2 documents)
+./index.html
+  error: bad link bar.html#goo
+
+Found 0 bad links
+Found 1 bad anchors
+"#,
+        );
+        site.close().unwrap();
+    }
+
+    #[test]
+    fn test_no_args() {
+        let mut cmd = Command::cargo_bin("hyperlink").unwrap();
+
+        cmd.assert()
+            .failure()
+            .code(1)
+            .stdout("")
+            .stderr(predicate::str::contains(
+                "\
+USAGE:
+    hyperlink [FLAGS] [OPTIONS] [base-path] [SUBCOMMAND]\
+",
+            ));
+    }
+}
