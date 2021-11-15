e3ace43b117b8101e1cf7e4c5d94123157026af2
Markus Unterwaditzer <markus-honeypot@unterwaditzer.net>
2021-11-15 14:57:08 +0100

    add release script

---
+++ scripts/release-version.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -xe
+
+[ -z "$(git status --porcelain)" ] || (echo "dirty working directory" && exit 1)
+
+current_version="$(grep '^version = ' Cargo.toml | head -1 | cut -d '"' -f2)"
+new_version="$1"
+
+action_prefix="untitaker\\/hyperlink@"
+sed -i '' "s/$action_prefix$current_version/$action_prefix$new_version/" README.md
+sed -i '' "s/version = \"$current_version\"/version = \"$new_version\"/" Cargo.toml
+cargo build
+
+git add README.md
+git add Cargo.toml
+git commit -am "version $new_version"
+git tag $new_version
+
+git show HEAD
+
+echo "now push:"
+echo "  git push"
+echo "  git push origin $new_version"
