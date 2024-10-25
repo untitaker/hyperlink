4e73292e11062e9e9ade3c5ba7a3613d73724114
Markus Unterwaditzer <markus-tarpit+git@unterwaditzer.net>
2024-10-25 21:48:27 +0200

    fix install.sh for github action

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,27 +1,7 @@
 #!/bin/sh
 set -e
-UNAME="`uname`"
-tag="`grep '"version":' ./package.json | cut -d'"' -f4`"
-echo "downloading hyperlink $tag for $UNAME"
-case $UNAME in
-    Linux) (
-        curl -L -o scripts/hyperlink-bin https://github.com/untitaker/hyperlink/releases/download/$tag/hyperlink-linux-x86_64
-    ) ;;
-    Darwin) (
-        UNAME_M="`uname -m`"
-        if [ "$UNAME_M" = "arm64" ]; then
-            curl -L -o scripts/hyperlink-bin https://github.com/untitaker/hyperlink/releases/download/$tag/hyperlink-mac-aarch64
-        else
-            curl -L -o scripts/hyperlink-bin https://github.com/untitaker/hyperlink/releases/download/$tag/hyperlink-mac-x86_64
-        fi
-    ) ;;
-    CYGWIN*) (
-        curl -L -o scripts/hyperlink-bin https://github.com/untitaker/hyperlink/releases/download/$tag/hyperlink-windows-x86_64.exe
-    ) ;;
-    *) (
-        echo "$UNAME not supported"
-        exit 1
-    )
-esac
+tag="`grep 'version = ' Cargo.toml | head -1 | cut -d'"' -f2`"
 
-chmod +x scripts/hyperlink-bin
+echo "downloading hyperlink $tag"
+
+curl --proto '=https' --tlsv1.2 -LsSf https://github.com/untitaker/hyperlink/releases/download/$tag/hyperlink-installer.sh | sh
